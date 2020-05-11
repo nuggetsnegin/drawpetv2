6c41c984f9c6e65d5ef1912f36ace70a62368673
Negin Sauermann <nuggetnegin@gmail.com>
2020-05-11 16:11:10 -0400

    quick colors done (might tweak pallet later..)

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -38,7 +38,7 @@
     </div>
 
     <div class="quick-colors" id="colors">
-      <button class="color-1 box" style="background: rgb(68, 67, 67)" @click="this.getQuickColor"></button>
+      <button class="color-1 box" style="background: #01BAEF" @click="this.getQuickColor"></button>
       <button class="color-2 box" style="background: rgb(228, 129, 231)" @click="this.getQuickColor"></button>
       <button class="color-3 box" style="background:rgb(106, 202, 130)" @click="this.getQuickColor"></button>
       <button class="color-4 box" style="background:rgb(240, 237, 99)" @click="this.getQuickColor"></button>
@@ -46,18 +46,18 @@
       <button class="color-6 box" style="background: rgb(112, 110, 235)" @click="this.getQuickColor"></button>
       <button class="color-7 box" style="background:rgb(232, 120, 68)" @click="this.getQuickColor"></button>
       <button class="color-8 box" style="background:rgb(240, 72, 86)" @click="this.getQuickColor"></button>
-      <button class="color-9 box" style="background:rgb(74, 169, 233)" @click="this.getQuickColor"></button>
-      <button class="color-10 box" style="background:rgb(106, 202, 130)" @click="this.getQuickColor"></button>
-      <button class="color-11 box" style="background: rgb(228, 129, 231)" @click="this.getQuickColor"></button>
-      <button class="color-12 box" style="background: rgb(228, 129, 231)" @click="this.getQuickColor"></button>
-      <button class="color-13 box" style="background: rgb(228, 129, 231)" @click="this.getQuickColor"></button>
-      <button class="color-14 box" style="background: rgb(228, 129, 231)" @click="this.getQuickColor"></button>
-      <button class="color-15 box" style="background: rgb(228, 129, 231)" @click="this.getQuickColor"></button>
-      <button class="color-16 box" style="background: rgb(228, 129, 231)" @click="this.getQuickColor"></button>
-      <button class="color-17 box" style="background: rgb(228, 129, 231)" @click="this.getQuickColor"></button>
-      <button class="color-19 box" style="background: rgb(228, 129, 231)" @click="this.getQuickColor"></button>
-      <button class="color-20 box" style="background: rgb(228, 129, 231)" @click="this.getQuickColor"></button>
-      <button class="color-21 box" style="background: rgb(228, 129, 231)" @click="this.getQuickColor"></button>
+       <button class="color-9 box" style="background: #0B4F6C" @click="this.getQuickColor"></button>
+     <button class="color-10 box" style="background: #1E3231" @click="this.getQuickColor"></button>
+      <button class="color-11 box" style="background: #5B5941" @click="this.getQuickColor"></button>
+      <button class="color-12 box" style="background: #2C2C54" @click="this.getQuickColor"></button>
+      <button class="color-13 box" style="background: #DABECA" @click="this.getQuickColor"></button>
+      <button class="color-14 box" style="background: #D3D4D9" @click="this.getQuickColor"></button>
+      <button class="color-15 box" style="background: #C6DEA6" @click="this.getQuickColor"></button>
+      <button class="color-16 box" style="background: #DDFDFE" @click="this.getQuickColor"></button>
+      <button class="color-17 box" style="background: #FFFEFF" @click="this.getQuickColor"></button>
+      <button class="color-19 box" style="background: #315659" @click="this.getQuickColor"></button>
+      <button class="color-20 box" style="background: #FF01FB" @click="this.getQuickColor"></button>
+      <button class="color-21 box" style="background: #26C485" @click="this.getQuickColor"></button>
     </div>
   </div>
 </template>
